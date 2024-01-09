1be5d82ee88f039b43254ffb98421f2bb096fa9b
Gabriel John <agjohngabriel@gmail.com>
2024-01-09 17:06:44 +0100

    new

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -12,8 +12,8 @@ import 'package:flutter/widgets.dart';
 class $AssetImageGen {
   const $AssetImageGen();
 
-  /// File path: asset/image/dog.jpeg
-  AssetGenImage get dog => const AssetGenImage('asset/image/dog.jpeg');
+  /// File path: asset/image/dog.png
+  AssetGenImage get dog => const AssetGenImage('asset/image/dog.png');
 
   /// List of all assets
   List<AssetGenImage> get values => [dog];
